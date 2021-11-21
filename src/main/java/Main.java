80ccffb31a6b11e82ff3e707265741e9e2662e43
Matty <m.jones.57@student.scu.edu.au>
2021-11-21 22:18:55 +1100

    Added a Person class for future use with comparing and sorting Can create Person objects from spreadsheet data (not functional yet)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -22,19 +22,28 @@ public class Main {
         FileReader fr = null;
         String line = null;
         Integer lineNumber = 0;
+        ArrayList<Person> people = new ArrayList<Person>();
         
-        System.out.println(Arrays.toString(args));
+        System.out.println(String.join(" ", args));
         
         // open the file
         try {
             
-            fr = new FileReader(args[0]);
+            fr = new FileReader(String.join(" ", args));
             
             // read the file
             BufferedReader buffer = new BufferedReader(fr);
             while((line = buffer.readLine()) != null) {
                 System.out.println(line);
                 lineNumber++;
+                // after the first line, create objects for each person
+                if (lineNumber > 1) {
+                    String[] data = line.split(",");
+                    for(String x: data) {
+                        System.out.println(x);
+                    }
+                    people.add(new Person(data));
+                }
                 System.out.println(lineNumber);
                 System.out.println("\n");
             }
---
+++ src/main/java/Person.java
@@ -0,0 +1,100 @@
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mathias Jones
+ */
+public class Person {
+    
+    // initialise variables
+    private String firstName;
+    private String lastName;
+    private Date date;
+    private Integer division;
+    private Integer points;
+    private String summary;
+    private SimpleDateFormat dateFormat;
+    
+    public Person(String[] data) throws ParseException {
+        
+        this.dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        
+        this.firstName = data[0];
+        this.lastName = data[1];
+        this.date = dateFormat.parse(data[2]);
+        this.division = Integer.parseInt(data[3]);
+        this.points = Integer.parseInt(data[4]);
+        this.summary = data[5];
+        
+    }
+    
+    /**
+     * Retrieves the persons first name
+     * @return - a string containing the first name
+     */
+    public String getFirstName() {
+        
+        return this.firstName;
+        
+    }
+    
+    /**
+     * Retrieves the persons last name
+     * @return - a string containing the last name
+     */
+    public String getLastName() {
+        
+        return this.lastName;
+        
+    }
+    
+    /**
+     * Retrieves the persons date
+     * @return - a date in the form of a date object
+     */
+    public Date getDate() {
+        
+        return this.date;
+        
+    }
+    
+    /**
+     * Retrieves the division that the person belongs to
+     * @return - an integer representing the division they belong to
+     */
+    public Integer getDivision() {
+        
+        return this.division;
+        
+    }
+    
+    /**
+     * Retrieves the number of points that the person scored
+     * @return - an integer that represents the number of points scored
+     */
+    public Integer getPoints() {
+        
+        return this.points;
+        
+    }
+    
+    /**
+     * Retrieves the description of the persons role
+     * @return - a string containing their role
+     */
+    public String getSummary() {
+        
+        return this.summary;
+        
+    }
+    
+}
